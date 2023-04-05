2182f33fd93fe051b40b4bf1e2b52c9910e8cd80
Alexandr Vinogradov <vinograder@yandex.ru>
2023-04-05 21:46:16 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 15_000;
+        int mile = 5;
+        int bonus = amount / mile;
+
+        System.out.println("Количество бонусных миль: " + bonus);
+    }
+}
